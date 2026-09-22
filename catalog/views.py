5eaae767a9a4f018f3d23406468689eda93a2021
Your Name <you@example.com>
2026-09-22 21:10:07 +0300

    Реализация  сообщения об успешной отправке данных

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Здесь мы возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'contacts.html')
